16c8992e59568ceeae2b1ff1c47ca789ad7208c7
Christelle Chatroux <christellechatroux@homair.com>
2019-05-20 17:35:25 +0200

    new commit, chapter not finished

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,16 @@
-const request = require('request')
+const request = require("request");
 
-const secretKey = 'b6acee7b148c3020c57e1712019f5380'
-const position = '37.8267,-122.4233'
-const urlBase = 'https://api.darksky.net/forecast/'
+const secretKey = "b6acee7b148c3020c57e1712019f5380";
+const position = "37.8267,-122.4233";
+const urlBase = "https://api.darksky.net/forecast/";
 
-const url = `${urlBase}${secretKey}/${position}`
+const url = `${urlBase}${secretKey}/${position}?units=si&language=fr`;
 
-
-request(url, (error, response, body) => {
-  // const data = JSON.parse(response.body) // that is the way used by the instructor
-  const data = JSON.parse(body)
-  console.log(data.currently)
+request({ url, json: true }, (error, response, body) => {
+  // console.log(response.body.currently) // that is the way used by the instructor
+  console.log(
+    `It is currently ${body.currently.temperature} degrees out. There is a ${
+      body.currently.precipProbability
+    }% chance of rain.`
+  );
 });
